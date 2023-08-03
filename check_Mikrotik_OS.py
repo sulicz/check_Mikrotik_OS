99faf810b1edc50a27c38febb9a04c1be21327cf
sulicz <sulaiman@itpe.cz>
2023-08-03 20:38:56 +0200

    Update check_Mikrotik_OS.py
    partial support of SNMP v3

--- check_Mikrotik_OS.py
+++ check_Mikrotik_OS.py
@@ -4,9 +4,10 @@
 import argparse
 import sys
 import datetime
-import urllib.request
+import urllib
 import ssl
-from packaging.version import Version, parse
+import time
+from distutils.version import StrictVersion
 
 #MIKROTIK-MIB::
 #MIKROTIK-MIB::mtxrLicVersion.0
@@ -16,8 +17,8 @@ OID_CurrentFirmwareVersion = '.1.3.6.1.4.1.14988.1.1.7.4.0'  # Current RouterOS 
 #MIKROTIK-MIB::mtxrFirmwareUpgradeVersion.0
 OID_LatestFirmwareVersion = '.1.3.6.1.4.1.14988.1.1.7.7.0'  # Latest availble RouterOS Firmware Version
 
-baseURLS = {"https://download.mikrotik.com/", "https://upgrade.mikrotik.com/" }
-debug = False;
+baseURLS = {"http://download.mikrotik.com/", "http://upgrade.mikrotik.com/" }
+debug = False
 
 def nagios_exit(overallStatus, message):
 
@@ -56,6 +57,9 @@ def getLatestMikrotik(ReleaseChannel):
         currentURL = "routeros/LATEST.6"
         bugFixOnlyURL = "routeros/LATEST.6fix"
         RCURL = "routeros/LATEST.6rc"
+        # for version 7
+        currentURL7 = "routeros/LATEST.7"
+
 
         if ReleaseChannel == "Current":
             URL = EachBaseURL + currentURL
@@ -63,19 +67,27 @@ def getLatestMikrotik(ReleaseChannel):
             URL = EachBaseURL + bugFixOnlyURL
         elif ReleaseChannel == "ReleaseCandidate":
             URL = EachBaseURL + RCURL
+        elif ReleaseChannel == "Current7":
+            URL = EachBaseURL + currentURL7
         else:
             nagios_exit("Unknown", "Unknown Mikrotik Release Channel")
 
         try:
+            for x in range(5):
+                try:
+                    with urllib.request.urlopen(URL, timeout=1) as response:
+                        ReturnedString = response.read()
+                        ReturnedString = ReturnedString.decode('utf-8')
+                        ReturnedString = ReturnedString.split()
+                        lastestVersion = ReturnedString[0]
+                        ReleaseDate = ReturnedString[1]
+                        return lastestVersion, ReleaseDate
+  
+                except:
+                    time.sleep(1) 
+                    pass
+                break
 
-            with urllib.request.urlopen(URL) as response:
-                ReturnedString = response.read()
-
-            ReturnedString = ReturnedString.decode('utf-8')
-            ReturnedString = ReturnedString.split()
-            lastestVersion = ReturnedString[0]
-            ReleaseDate = ReturnedString[1]
-            return lastestVersion, ReleaseDate
         except ssl.CertificateError as e:
             debugMessage(URL)
             error =  e
@@ -99,10 +111,21 @@ def getSNMP_OID(IPAddress, Version, SNMPOptions, OID):
     if Version == "2c":
         g = getCmd(SnmpEngine(),
                    CommunityData(SNMPOptions['Community']),
-                   UdpTransportTarget((IPAddress, 161)),
+                   UdpTransportTarget((IPAddress, SNMPOptions['Port'])),
                    ContextData(),
                    ObjectType(ObjectIdentity(OID))
                    )
+
+    if Version == "3":
+        g = getCmd(SnmpEngine(),
+                   UsmUserData(SNMPOptions['Community'], SNMPOptions['AuthPass'], SNMPOptions['EncPass'],
+                        authProtocol=usmHMACSHAAuthProtocol,
+                        privProtocol=usmAesCfb128Protocol),
+                   UdpTransportTarget((IPAddress, SNMPOptions['Port'])),
+                   ContextData(),
+                   ObjectType(ObjectIdentity(OID))
+                   )
+
 
     errorIndication, errorStatus, errorIndex, varBinds = next(g)
     if errorIndication is None:
@@ -119,6 +142,9 @@ def process_cli():
     parser.add_argument('-H', '--hostIP',
                          help='Mikrotik Router IP Address',
                          type=str, required=True)
+    parser.add_argument('-p', '--port',
+                         help='Mikrotik Router SNMP port',
+                         type=str, required=False)
     parser.add_argument('-v', '--snmpVersion',
                          help='SNMP Version (only 2c currently supported) ',
                          type=str, required=True)
@@ -126,9 +152,15 @@ def process_cli():
                          help='SNMP Community String (only v1 and 2c) ', type=str, required=False)
     parser.add_argument('-c', '--mikrotikReleaseChannel',
                          help='The mikrotik release channel to check for updates against: Bugfix, Current or \
-                         ReleaseCandidate',
+                         ReleaseCandidate, for V7 Current7',
                          type=str, required=True)
-
+    parser.add_argument('-F', '--noFirmware',
+                         help='Ignore firmware (for non-routerboard instalation - CHR) ', action="store_true")
+    parser.add_argument('-A', '--authPass',
+                         help='SNMP authentication protocol pass phrase ', type=str, required=False)
+    parser.add_argument('-X', '--encPass',
+                         help='SNMP encryption/privacy protocol pass phrase ', type=str, required=False)
+ 
     # Parse arguments and die if error
     try:
         args = parser.parse_args()
@@ -139,6 +171,14 @@ def process_cli():
         print ("SNMP Community string required for SNMP Versions other than v3.")
         sys.exit(3)
 
+    if args.snmpVersion == "3" and  (not args.authPass or not args.encPass):
+        print ("SNMP v3 use SHA1 and AES in mode authPriv (Security: private) it must be set -A and -X")
+        sys.exit(3)
+
+
+    if not args.port:
+        args.port=161
+
     return args
 
 
@@ -147,7 +187,7 @@ def OutOfDate(CurrentVersionString, LatestVersionString):
     try:
         CurrentVersionString = str(CurrentVersionString)
         CurrentVersionString = CurrentVersionString.replace('rc', 'b')
-        CurrentVersion = Version(str(CurrentVersionString))
+        CurrentVersion = StrictVersion(str(CurrentVersionString))
 
     except:
         message = "Don't know how to handle current version number"
@@ -159,7 +199,7 @@ def OutOfDate(CurrentVersionString, LatestVersionString):
              raise
         LatestVersionString = str(LatestVersionString)
         LatestVersionString = LatestVersionString.replace('rc', 'b')
-        LatestVersion = Version(LatestVersionString)
+        LatestVersion = StrictVersion(LatestVersionString)
     except:
         message = "Don't know how to handle latest number"
         nagios_exit('Unknown', message)
@@ -187,7 +227,13 @@ def statusCheck(currentStatus, NewStatus):
 def main():
 
     args = process_cli()
-    SNMPOptions = {'Community': args.snmpCommunity}
+    SNMPOptions = {'Community': args.snmpCommunity, 
+            'Port': args.port,
+            'AuthPass': args.authPass,
+            'EncPass': args.encPass
+            }
+
+
 
     # Get information from SNMP and mikrotik websites about version mumbers
     LatestRouterOSVersion, LatestRouterOSVersion_ReleaseDate = (getLatestMikrotik(args.mikrotikReleaseChannel))
@@ -212,12 +258,17 @@ def main():
 
     # Determine current Firmware Status
 
-    if OutOfDate(CurrentFirmwareVersion, LastestFirmwareVersion):
-        firmwareMessage = "Firmware Requires Upgrade (" + CurrentFirmwareVersion + " -> " + LastestFirmwareVersion + ")."
-        firmwareStatus = "Critical"
+    if not args.noFirmware:
+        if OutOfDate(CurrentFirmwareVersion, LastestFirmwareVersion):
+            firmwareMessage = "Firmware Requires Upgrade (" + CurrentFirmwareVersion + " -> " + LastestFirmwareVersion + ")."
+            firmwareStatus = "Critical"
+        else:
+            firmwareMessage = "Firmware is up to date (" + CurrentFirmwareVersion + ")."
+            firmwareStatus = "Ok"
     else:
-        firmwareMessage = "Firmware is up to date (" + CurrentFirmwareVersion + ")."
         firmwareStatus = "Ok"
+        CurrentFirmwareVersion = "Firmware check disabled -F"
+        firmwareMessage = CurrentFirmwareVersion
 
     # Figure out overall status
 
@@ -226,7 +277,7 @@ def main():
     if overallStatus == "Ok":
         message = "RouterOS and Firmware is up to date" + " (" + CurrentRouterOsVerion + "/" \
                    + CurrentFirmwareVersion + ")."
-        nagios_exit("OK", message)
+        nagios_exit("Ok", message)
     else:
         nagios_exit(overallStatus, RouterOSMessage + " " + firmwareMessage)
 
